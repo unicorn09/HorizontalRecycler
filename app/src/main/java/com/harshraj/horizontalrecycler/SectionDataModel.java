ff923b2102a3d68c3fa39db85cea378971d75f9e
DELL <harshrajnit@gmail.com>
2019-10-20 11:23:50 +0530

    Initial commit

---
+++ app/src/main/java/com/harshraj/horizontalrecycler/SectionDataModel.java
@@ -0,0 +1,38 @@
+package com.harshraj.horizontalrecycler;
+
+import java.util.ArrayList;
+
+public class SectionDataModel {
+
+
+
+    private String headerTitle;
+    private ArrayList<SingleItemModel> allItemsInSection;
+
+
+    public SectionDataModel() {
+
+    }
+    public SectionDataModel(String headerTitle, ArrayList<SingleItemModel> allItemsInSection) {
+        this.headerTitle = headerTitle;
+        this.allItemsInSection = allItemsInSection;
+    }
+
+
+
+    public String getHeaderTitle() {
+        return headerTitle;
+    }
+
+    public void setHeaderTitle(String headerTitle) {
+        this.headerTitle = headerTitle;
+    }
+
+    public ArrayList<SingleItemModel> getAllItemsInSection() {
+        return allItemsInSection;
+    }
+
+    public void setAllItemsInSection(ArrayList<SingleItemModel> allItemsInSection) {
+        this.allItemsInSection = allItemsInSection;
+    }
+}
